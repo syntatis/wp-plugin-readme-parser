ef534d17f29e97f155627ec7df255ef763f3d2b5
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-04-15 19:30:10 +0700

    Update test to cover "Tested" header alternative

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -21,5 +21,24 @@ class ParserTest extends TestCase
 		$parser = new Parser(__DIR__ . '/fixtures/readme.txt');
 
 		$this->assertEquals('5.4', $parser->tested);
+
+		$parser = new Parser(__DIR__ . '/fixtures/readme-alt1.txt');
+
+		$this->assertEquals('5.0', $parser->tested);
+	}
+
+	/**
+	 * Test retrieving the "Tested up to:" value from the Markdown file.
+	 *
+	 * @return void
+	 */
+	public function testRetrievingTestedMarkdown() {
+		$parser = new Parser(__DIR__ . '/fixtures/readme.md');
+
+		$this->assertEquals('3.0', $parser->tested);
+
+		$parser = new Parser(__DIR__ . '/fixtures/readme-alt1.md');
+
+		$this->assertEquals('6.0', $parser->tested);
 	}
 }
