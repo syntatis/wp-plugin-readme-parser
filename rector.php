399b814e0ee68730eeaacdba0f3a2befc63e8d8f
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-08-03 13:26:34 +0700

    Repository chores (#36)

--- rector.php
+++ rector.php
@@ -1,21 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Arguments\Rector\FuncCall\FunctionArgumentDefaultValueReplacerRector;
 use Rector\Arguments\ValueObject\ReplaceFuncCallArgumentDefaultValue;
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
+return static function (RectorConfig $rectorConfig) {
 	$rectorConfig->parallel();
 	$rectorConfig->importNames();
-    $rectorConfig->paths([__DIR__ . '/app']);
-    $rectorConfig->sets([
-		DowngradeLevelSetList::DOWN_TO_PHP_70,
-    ]);
-	$rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
-		'WordPressdotorg\Plugin_Directory\Markdown' => 'Syntatis\WPPluginReadMeParser\Markdown',
+	$rectorConfig->paths([__DIR__ . '/app']);
+	$rectorConfig->sets([
+		DowngradeLevelSetList::DOWN_TO_PHP_74,
 	]);
+	$rectorConfig->ruleWithConfiguration(RenameClassRector::class, ['WordPressdotorg\Plugin_Directory\Markdown' => 'Syntatis\WPPluginReadMeParser\Markdown']);
 	$rectorConfig->ruleWithConfiguration(FunctionArgumentDefaultValueReplacerRector::class, [
 		new ReplaceFuncCallArgumentDefaultValue(
 			'class_exists',
