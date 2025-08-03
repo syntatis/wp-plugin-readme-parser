0be98c7bbf924daf625ce84f6265874d2e4ae4d0
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-08-03 11:32:12 +0700

    Fix PHPCS fules

--- rector.php
+++ rector.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Arguments\Rector\FuncCall\FunctionArgumentDefaultValueReplacerRector;
 use Rector\Arguments\ValueObject\ReplaceFuncCallArgumentDefaultValue;
 use Rector\Config\RectorConfig;
@@ -9,13 +11,11 @@ use Rector\Set\ValueObject\DowngradeLevelSetList;
 return static function (RectorConfig $rectorConfig): void {
 	$rectorConfig->parallel();
 	$rectorConfig->importNames();
-    $rectorConfig->paths([__DIR__ . '/app']);
-    $rectorConfig->sets([
+	$rectorConfig->paths([__DIR__ . '/app']);
+	$rectorConfig->sets([
 		DowngradeLevelSetList::DOWN_TO_PHP_70,
-    ]);
-	$rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
-		'WordPressdotorg\Plugin_Directory\Markdown' => 'Syntatis\WPPluginReadMeParser\Markdown',
 	]);
+	$rectorConfig->ruleWithConfiguration(RenameClassRector::class, ['WordPressdotorg\Plugin_Directory\Markdown' => 'Syntatis\WPPluginReadMeParser\Markdown']);
 	$rectorConfig->ruleWithConfiguration(FunctionArgumentDefaultValueReplacerRector::class, [
 		new ReplaceFuncCallArgumentDefaultValue(
 			'class_exists',
